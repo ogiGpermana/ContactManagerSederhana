70d8e498ff88c76b68e5276ee0228a150478a610
Ogi G Permana <hunterdumay@gmail.com>
2017-06-24 02:04:47 +0700

    Membuat Data Grup seeder(Hard Code)

---
+++ database/seeds/GroupsTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class GroupsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('groups')->delete();
+
+        $groups = [
+            [
+                'id' => 1,
+                'name' => 'Keluarga',
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime
+            ],
+            [
+                'id' => 2,
+                'name' => 'Teman',
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime
+            ],
+            [
+                'id' => 3,
+                'name' => 'Lain lain',
+                'created_at' => new DateTime,
+                'updated_at' => new DateTime
+            ]
+        ];
+
+        DB::table('groups')->insert($groups);
+    }
+}
