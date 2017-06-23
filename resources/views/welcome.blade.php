5eda93a393388cfe9e33f2e3566012c770440679
Ogi G Permana <hunterdumay@gmail.com>
2017-06-23 19:14:11 +0700

    Modif tampilan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
+        <link href="{{ asset('font-awesome/css/font-awesome.css') }}" rel="stylesheet">
         <style>
             html, body {
                 background-color: #fff;
@@ -66,8 +67,13 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
+            <div class="content">
+                <div class="title m-b-md">
+                    <i class="fa fa-fw fa-pencil"></i><i class="fa fa-fw fa-book"></i><br>
+                    Contact Manager v.0.1
+                </div>
+                @if (Route::has('login'))
+                <div class="links">
                     @if (Auth::check())
                         <a href="{{ url('/home') }}">Home</a>
                     @else
@@ -75,20 +81,7 @@
                         <a href="{{ url('/register') }}">Register</a>
                     @endif
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                @endif
             </div>
         </div>
     </body>
