96d4beaab768032b5ebacbb2c356a8c8c8368bd9
Ogi G Permana <hunterdumay@gmail.com>
2017-06-24 11:30:22 +0700

    Pemanggilan model contact

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ContactManager\Http\Controllers;
+
+use Illuminate\Http\Request;
+use ContactManager\Contact;
+
+class ContactsController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contacts = Contact::all();
+        return view('contacts.index', compact('contacts'));
+    }
+
+    public function test()
+    {
+        return view('contacts.form');
+    }
+}
