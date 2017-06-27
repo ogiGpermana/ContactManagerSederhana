e501a573c275c4dfcba014e10a0b315923450ea3
Ogi G Permana <hunterdumay@gmail.com>
2017-06-27 22:40:22 +0700

    Menambahkan variabel  untuk validasi input

--- app/Contact.php
+++ app/Contact.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
+    protected $fillable = [
+      'name', 'email', 'company', 'phone', 'address', 'group_id'
+    ];
     public function group()
     {
       return $this->belongsTo('ContactManager\Group');
