c36cd39274b8aa6d6a2a8298e79f65e5c068a862
Ogi G Permana <hunterdumay@gmail.com>
2017-06-27 22:43:12 +0700

    Menambahkan rute fungsi store dan create untuk menyimpan ke database

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -4,6 +4,7 @@ namespace ContactManager\Http\Controllers;
 
 use Illuminate\Http\Request;
 use ContactManager\Contact;
+use ContactManager\Group;
 
 class ContactsController extends Controller
 {
@@ -26,17 +27,37 @@ class ContactsController extends Controller
     {
         if (($group_id = $request->get("group_id")))
         {
-          $contacts = Contact::where('group_id', $group_id)->paginate(5);
+          $contacts = Contact::where('group_id', $group_id)->oderBy('id', 'desc')->paginate(5);
         }
         else {
-          $contacts = Contact::paginate(5);
+          $contacts = Contact::oderBy('id', 'desc')->paginate(5);
         }
 
         return view('contacts.index', compact('contacts'));
     }
 
-    public function test()
+    public function create()
     {
-        return view('contacts.form');
+        $groups = [];
+        foreach(Group::all() as $group)
+        {
+            $groups[$group->id] = $group->name;
+        }
+        return view('contacts.create', compact('groups'));
+    }
+
+    public function store(Request $request)
+    {
+        $rules = [
+            'name' => ['required', 'min:5'],
+            'email' => ['required', 'email'],
+            'company' => ['required']
+        ];
+
+        $this->validate($request, $rules);
+
+        Contact::create($request->all());
+
+        return redirect("contact")->with("message", "Kontak berhasil di simpan!");
     }
 }
