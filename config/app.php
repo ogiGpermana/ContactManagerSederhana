01e1289cb768e14902766964d2943bf4d43c3510
Ogi G Permana <hunterdumay@gmail.com>
2017-06-27 22:35:35 +0700

    Menambahkan library html form laravel collective

--- config/app.php
+++ config/app.php
@@ -136,6 +136,7 @@ return [
     */
 
     'providers' => [
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Laravel Framework Service Providers...
@@ -191,6 +192,8 @@ return [
     */
 
     'aliases' => [
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
         'App' => Illuminate\Support\Facades\App::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
